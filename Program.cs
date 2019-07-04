abdedff935eb633871806a9e0e3fc5f60825371b
gugu <gustavocorrea2032@gmail.com>
2019-07-04 12:25:14 -0300

    Criação e utilização da função dataHora ; Colocada uma descrição na função

--- Program.cs
+++ Program.cs
@@ -4,9 +4,19 @@ namespace FSRSA
 {
     class Program
     {
-        static void Main(string[] args)
-        {
-           
+        static void Main(string[] args){
+
+        dataHora();   
+
+        }
+        /// <summary>
+        /// A função dataHora exibe na tela de console a data e a hora do sistema. 
+        /// </summary>
+        
+        static void dataHora(){
+
+            Console.WriteLine(DateTime.Now);
+                    
         }
     }
 }
